887421914032ef5c09af55e6c09e500fe539d2d4
deroux <dominicrudigier@gmail.com>
2023-07-05 09:45:52 +0200

    Setup everything for productivity system, git pre-commit and pre-push hooks using prettier, eslint and test

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({})),
-    provideRouter(routes),
-  ],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        importProvidersFrom(IonicModule.forRoot({})),
+        provideRouter(routes),
+    ],
 });
